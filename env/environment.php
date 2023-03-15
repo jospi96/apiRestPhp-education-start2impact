3fcd79acb75f7fef21b42290c36edde5ac99eb54
jospi96 <113528331+jospi96@users.noreply.github.com>
2023-03-15 17:43:36 +0100

    Delete env directory

--- env/environment.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("db_url","127.0.0.1");
-define("db_name","education");
-define("db_user","root");
-define("db_user_password","");
-define("table_course_name","courses");
-define("table_subjects_name","subjects");
-define("table_course_subjects_name","courses_subjects");
